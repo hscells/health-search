b58ba83ddc79fcccaa3faf3c6a1c764b8d6467ca
Harry Scells <harryscells@gmail.com>
2015-09-18 10:48:07 +1000

    removed default test

--- test/health_search/core_test.clj
+++ test/health_search/core_test.clj
@@ -1,7 +1,3 @@
 (ns health-search.core-test
   (:require [clojure.test :refer :all]
             [health-search.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
