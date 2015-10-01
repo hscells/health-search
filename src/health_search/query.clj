269fee8472315f0daf4d125a2f7d79c18f03b58e
Harry Scells <harryscells@gmail.com>
2015-10-01 20:49:59 +1000

    implementation of some term association measures

--- src/health_search/query.clj
+++ src/health_search/query.clj
@@ -4,7 +4,44 @@
             [clojurewerkz.elastisch.rest.document :as esd]
             [clojurewerkz.elastisch.query         :as q]
             [clojurewerkz.elastisch.rest.response :as esrsp]
+            [clojure.string                       :as str]
             [clojure.pprint :as pp]))
+
+(defn prob
+  "calculate the probability of n gram x in terms n"
+  [x n]
+  (/ (count (filter x n)) (count n)))
+
+(defn prob
+  "calculate the probability of n grams x and y in terms n"
+  [x y n]
+  (/ (+ (count (filter x n) (count (filter y n)))) (count n)))
+
+(defn dice
+  "implementation of Dice-coefficient"
+  [a, b]
+  (/ (* 2 (* a b)) (+ a b)))
+
+(defn pmi
+  "implementation of Pointwise Mututal Information"
+  [a b n]
+  (Math/log (/ (prob a b) (* (prob a ) (prob b)))))
+
+(defn emim
+  "implementation of Expected Mutual Information Measure"
+  [a b n]
+  (* (prob a b n) (pmi a b n))
+
+(defn expand-dice
+  "query expansion function using Dice-coefficient"
+  [terms]
+  (map ))
+
+(defn expand-query
+  "given a query string, apply a query expanding function to it"
+  [query function]
+  (let [terms (str/split query #" ") expanded-query (apply function terms)]
+    expanded-query))
 
 (defn search
   [query]
