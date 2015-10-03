05e8001700482060e9c64cee474a1a7fbcff5d02
Harry Scells <harryscells@gmail.com>
2015-10-03 16:01:55 +1000

    added html parser

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clojurewerkz/elastisch "2.1.0"]]
+                 [clojurewerkz/elastisch "2.1.0"]
+                 [enlive "1.1.6"]]
   :main ^:skip-aot health-search.core
   :target-path "target/%s"
   :plugins [[codox "0.8.13"]]
