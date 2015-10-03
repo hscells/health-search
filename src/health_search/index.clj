89a182a751e10dc9a67950ab35264662d4cefa87
Harry Scells <harryscells@gmail.com>
2015-10-03 16:04:11 +1000

    apply html stripping to documents

--- src/health_search/index.clj
+++ src/health_search/index.clj
@@ -1,23 +1,50 @@
 (ns health-search.index
-  (:require [health-search.connection :as connection]
-            [clojurewerkz.elastisch.rest :as esr]
+  (:require [health-search.connection             :as connection]
+            [clojurewerkz.elastisch.rest          :as esr]
             [clojurewerkz.elastisch.rest.document :as esd]
-            [clojure.java.io :as io]))
+            [clojurewerkz.elastisch.rest.index    :as esi]
+            [clojure.string                       :as str]
+            [clojure.java.io                      :as io]
+            [net.cgrand.enlive-html               :as html]))
+
+;; This code is credited to:
+;; http://shenfeng.me/using-tagsoup-extract-text-from-html.html
+(defn- emit-str [node]
+   (cond (string? node) node
+         (and (:tag node)
+              (not= :script (:tag node))) (emit-str (:content node))
+         (seq? node) (map emit-str node)
+         :else ""))
+
+ (defn extract-text [html]
+   (when html
+     (let [r (html/html-resource (java.io.StringReader. html))]
+       (str/trim (apply str (flatten (emit-str r)))))))
 
 (defn add-to-index
   "Add a document to the index with a name, an id with the elstic search connection"
   [name id conn]
   (println name)
-  (esd/put conn "health-search" "document" id {:name id :text (slurp name)}))
+  (let [html (slurp name)]
+    (esd/put conn "health-search" "document" id {:title (get (first (html/select (html/html-resource (java.io.StringReader. html)) [:title])) :content) :text (extract-text html)})))
 
 (defn index-collection
   "take a directory and index it on elastic search"
   [options]
   ; connect to the elastic search instance
-  (let [corpus (first options) conn (esr/connect (connection/config :host)) id 0
-        mapping-types {"document" {:properties {
-                                    :name {:type "string" :store "yes"}
-                                    :text {:type "string" :index "BM25"}}}}]
+  (let [corpus (first options) conn (esr/connect (connection/config :host))]
+    (esi/create conn "health-search"
+            :settings {:index {:analysis {:analyzer {:custom_health {
+                                                                     :type         "custom"
+                                                                     :tokenizer    "standard"
+                                                                     :char_filter  "html_strip"
+                                                                     :filter       ["standard" "lowercase"]}}}}}
+            :mappings {
+              :document {
+                :_all {:enabled "true"}
+                :properties {
+                  :text  {:type "string" :analyzer "custom_health" :search_analyzer "custom_health" :store "no" :index "analyzed"}
+                  :title {:type "string"}}}})
     (cond
       (nil? corpus) nil
       :else
